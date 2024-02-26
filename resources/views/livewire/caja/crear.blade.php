041f344d0edd07981919b33cbaf3cbb958ea3684
David Lazarte <dav_lazarte@hotmail.com>
2024-02-26 16:38:16 -0300

    solucionado los modales de creacion de ingresos,salidas y caja

--- resources/views/livewire/caja/crear.blade.php
+++ resources/views/livewire/caja/crear.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed z-10 inset-0 overflow-y-auto ease-out duration-400">
+<div class="fixed inset-0 z-50 overflow-y-auto">
     <div class="flex justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
 
         <div class="fixed inset-0 transition-opacity">
@@ -6,7 +6,7 @@
         </div>
 
         <span class="hidden sm:inline-block sm:align-middle sm:h-screen"></span>
-        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-6xl sm:w-full"
+        <div class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-4xl sm:w-full"
             role="dialog" aria-modal="true" aria-labelledby="modal-headline">
             <div
                 class="flex  items-center border-l-8 border-emerald-500 bg-emerald-50 p-4 text-emerald-900 shadow-lg mt-6  col-span-12 sm:col-span-12">
